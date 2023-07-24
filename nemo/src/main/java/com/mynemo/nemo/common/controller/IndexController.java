89e08faab093026fdb707888156b186f265cc1db
minnnaa-p <minaproject01@gmail.com>
2023-07-24 17:18:27 +0900

    indexTest
    mariadb 확인용

--- nemo/src/main/java/com/mynemo/nemo/common/controller/IndexController.java
+++ nemo/src/main/java/com/mynemo/nemo/common/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.mynemo.nemo.common.controller;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,4 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface IndexController {
     public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception;
+    
+    public ModelAndView indexTest(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
---
+++ nemo/src/main/webapp/WEB-INF/views/indexTest.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"
+    isELIgnored="false"
+%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:set var="contextPath" value="${pageContext.request.contextPath}" />
+<%
+	request.setCharacterEncoding("utf-8");
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>네모: 동네모임</title>
+
+<script src="https://kit.fontawesome.com/f9a2702e84.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<h1>출력되나??</h1>
+</body>
+</html>
